36dbb6c8a770ec0af17bb0d744640289d894afaf
Vedaste MUGABO <125447762+MugaboVedaste@users.noreply.github.com>
2025-12-18 10:44:34 +0200

    Update ALLOWED_HOSTS for production deployment

--- PAM/PAM/settings.py
+++ PAM/PAM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-29q0^eregfu!p=ly*gfr#$*wimasj2@+xricy8n&liyf)8@fyq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com",]
 
 
 # Application definition
